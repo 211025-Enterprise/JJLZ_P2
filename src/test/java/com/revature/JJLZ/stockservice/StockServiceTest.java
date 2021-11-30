48dadf5915606293e59acc708e10c8e82fa9e075
Your Name <you@example.com>
2021-11-30 16:23:29 -0500

    api methods added in StockService

--- src/test/java/com/revature/JJLZ/stockservice/StockServiceTest.java
+++ src/test/java/com/revature/JJLZ/stockservice/StockServiceTest.java
@@ -4,6 +4,10 @@ import com.revature.JJLZ.stockmodel.StockWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -12,8 +16,17 @@ class StockServiceTest {
     private StockService stockService;
 
     @Test
-    void invoke(){
+    void invoke() throws IOException {
         final StockWrapper stock = stockService.findStock("AMZN");
         System.out.println(stock.getStock());
+
+        final BigDecimal price = stockService.findPrice(stock);
+        System.out.println("current price "+price);
+
+        final BigDecimal change = stockService.findPercentChange(stock);
+        System.out.println("change in percentage "+change+"%");
+
+        final BigDecimal percent200dma = stockService.findChangein200MAPercent(stock);
+        System.out.println("change in 200 day moving average "+percent200dma+"%");
     }
 }
