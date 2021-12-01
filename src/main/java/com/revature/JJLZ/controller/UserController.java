80e015becf678112cb15ffeb2d37d18564e73170
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2021-11-30 22:59:33 -0500

    Update UserController.java

--- src/main/java/com/revature/JJLZ/controller/UserController.java
+++ src/main/java/com/revature/JJLZ/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestControlle
+@RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
