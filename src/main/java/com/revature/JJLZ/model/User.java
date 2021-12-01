1c788ed114e90371da38495c606b81a0782337d8
levansiradze <levansiradze@yahoo.com>
2021-12-01 10:19:20 -0500

    stuff

--- src/main/java/com/revature/JJLZ/model/User.java
+++ src/main/java/com/revature/JJLZ/model/User.java
@@ -3,6 +3,7 @@ package com.revature.JJLZ.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
+import yahoofinance.Stock;
 
 import javax.persistence.*;
 import java.util.List;
@@ -28,6 +29,8 @@ public class User {
     private String password;
     @Column(nullable = false)
     private double balance;
+
+    public List<Stock> holding;
 
     @OneToMany(mappedBy = "watcher")
     @JsonManagedReference
