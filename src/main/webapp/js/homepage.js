01eadba8039849df078a4b5b9b42affc0518be73
Krakenmeister <johndmeo@gmail.com>
2021-12-02 06:11:46 +0000

    Homepage completed except for post requests

--- src/main/webapp/js/homepage.js
+++ src/main/webapp/js/homepage.js
@@ -5,6 +5,24 @@ var contentWrapper = document.getElementById('content-wrapper')
 
 function createForm() {
 	removeAllChildNodes(contentWrapper)
+	newElement('p', 'title', contentWrapper)
+	newElement('form', 'createForm', contentWrapper)
+	var form = document.getElementById('createForm')
+	form.setAttribute('class', 'homepageForm')
+	createInput('text', 'fname', form, "First Name:")
+	createInput('text', 'lname', form, "Last Name:")
+	createInput('text', 'uname', form, "New Username:")
+	createInput('password', 'pass', form, "New Password:")
+	newElement('div', 'formControl', form)
+	var formControl = document.getElementById('formControl')
+	newElement('p', 'back', formControl)
+	newElement('p', 'submit', formControl)
+	newElement('p', 'spacer', formControl)
+	document.getElementById('back').onclick = function() {goHome()}
+	document.getElementById('submit').onclick = function() {createUser()}
+
+	document.getElementById('title').style.marginBottom = '2rem'
+	addBlurryBackground(form, contentWrapper)
 }
 
 function logInForm() {
@@ -12,10 +30,19 @@ function logInForm() {
 	newElement('p', 'title', contentWrapper)
 	newElement('form', 'logInForm', contentWrapper)
 	var form = document.getElementById('logInForm')
+	form.setAttribute('class', 'homepageForm')
 	createInput('text', 'username', form, "Username:")
 	createInput('password', 'password', form, "Password:")
-	newElement('p', 'submit', form)
+	newElement('div', 'formControl', form)
+	var formControl = document.getElementById('formControl')
+	newElement('p', 'back', formControl)
+	newElement('p', 'submit', formControl)
+	newElement('p', 'spacer', formControl)
+	document.getElementById('back').onclick = function() {goHome()}
 	document.getElementById('submit').onclick = function() {logIn()}
+
+	document.getElementById('title').style.marginBottom = '2rem'
+	addBlurryBackground(form, contentWrapper)
 }
 
 function goHome() {
@@ -52,6 +79,22 @@ function createInput(type, id, formElement, labelString) {
 	textWrapper.appendChild(textLabel)
 	textWrapper.appendChild(textField)
 	formElement.appendChild(textWrapper)
+}
+
+function addBlurryBackground(formElement, parentElement) {
+	var rect = formElement.getBoundingClientRect()
+	var formBackground = document.createElement('div')
+	formBackground.setAttribute('id', 'formBackground')
+	formBackground.style.left = rect.left + 'px'
+	formBackground.style.top = rect.top + 'px'
+	formBackground.style.width = (rect.right - rect.left) + 'px'
+	formBackground.style.height = (rect.bottom - rect.top) + 'px'
+	formBackground.style.setProperty('--formPos', '-' + rect.left + 'px -' + rect.top + 'px')
+	parentElement.appendChild(formBackground)
+}
+
+function createUser() {
+
 }
 
 function logIn() {
